8eb349456203b0d8e13463d07a567f2731071af6
kimverweij <kimmverwey@gmail.com>
2024-06-04 10:00:23 +0200

    Added character, move mechanics, jump mechanic, animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private float rotSpeed = 25f;
+    public float walkSpeed = 3f; // Speed for walking
+    public float runSpeed = 6f; // Speed for running
+    public float smoothingFactor = 0.5f; // Smoothing factor for movement and rotation
+
+    private Vector3 movement;
+
 
     private Rigidbody rb;
+
+    private PlayAnimations playAnimations;
+
+    private JumpMechanic jumpMechanic;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        rb             = gameObject.GetComponent<Rigidbody>();
+        jumpMechanic   = gameObject.GetComponent<JumpMechanic>();
+        playAnimations = gameObject.GetComponent<PlayAnimations>();
     }
 
     // Update is called once per frame
@@ -22,29 +32,60 @@ public class PlayerController : MonoBehaviour
         
     }
 
+
+
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        bool isShiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
-        Debug.Log(moveHorizontal);
-        Debug.Log(moveVertical);
+        float currentSpeed = isShiftPressed ? runSpeed : walkSpeed;
 
-        
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized * currentSpeed;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized * speed;
-
-        // Bereken de nieuwe positie van het object
+        // Calculate the new position of the object
         Vector3 newPosition = rb.position + movement * Time.fixedDeltaTime;
 
-        // Gebruik Vector3.Lerp voor een vloeiende overgang naar de nieuwe positie
-        rb.MovePosition(Vector3.Lerp(rb.position, newPosition, 0.5f));
+        // Use Vector3.Lerp for a smooth transition to the new position
+        rb.MovePosition(Vector3.Lerp(rb.position, newPosition, smoothingFactor));
 
-        if (moveHorizontal != 0 || moveVertical != 0)
+        if (movement != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(moveHorizontal, moveVertical) * Mathf.Rad2Deg;
             Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
-            rb.MoveRotation(Quaternion.Lerp(rb.rotation, targetRotation, 0.5f));
+            rb.MoveRotation(Quaternion.Lerp(rb.rotation, targetRotation, smoothingFactor));
+
+            // player is moving, should not interfere animation while jumping
+            if (!jumpMechanic.IsJumping)
+            {
+                if (isShiftPressed)
+                {
+                    playAnimations.ChangeAnimationState("Run");
+                }
+                else
+                {
+                    playAnimations.ChangeAnimationState("Walk");
+                }
+                
+            }
         }
+        else // player is not moving
+        {
+            if (!jumpMechanic.IsJumping) // and not jumping
+            {
+                playAnimations.ChangeAnimationState("Idle");
+            }
+        }
+
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                Debug.Log("Key pressed: " + key);
+            }
+        }
+        Debug.Log(movement);
     }
 }
+
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    // Rotation speed
+    public float rotationSpeed = 30.0f;
+
+    void Update()
+    {
+        // Rotate the object around its up axis
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+    }
+}
